f9ace49c055be9dde6c0c881ee1862f08cb0cede
orof84 <orof84@gmail.com>
2022-04-13 15:21:25 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# C_project_test
+
+1. clone project (in the next stages the path of the cloned folder will be called "repository path").
+2. open spkmeans_compare.py.
+3. open cmd and check if the command "python3" works. if not, change YOUR_PYTHON_COMMAND constant in spkmeans_compare.py to be the correct command name you use to run python from cmd.
+4. change FINAL_PROJECT_PATH to be your final project path that includes the spkmeans.py file (the r is necessary, the path should look like - r"your_path") 
+5. change TEST_PROJECT_PATH to be your repository path.
+6. run "python setup.py build_ext --inplace" as usual to get the most recent module.
+7. copy the module build dir and .so file to the repository path.
+8. compile and copy spkmeans file to the repository path.
+9. open cmd from the repository path.
+10. run "python spkmeans_compare.py"
+
+your tests results will be shown on the console and as a results file at "test_results" directory.
---
+++ spkmeans_compare.py
@@ -0,0 +1,55 @@
+import os, sys, json
+from datetime import datetime
+
+FINAL_PROJECT_PATH = r'your_project'
+TEST_PROJECT_PATH = r'your_tests'
+INPUTS_PATH = os.path.join(TEST_PROJECT_PATH, "tests_inputs_outputs/inputs")
+RESULTS_PATH = os.path.join(TEST_PROJECT_PATH, "tests_inputs_outputs/outputs")
+TEST_RESULTS_PATH = os.path.join(TEST_PROJECT_PATH, "test_results")
+YOUR_RESULTS_PATH = os.path.join(TEST_PROJECT_PATH, "your_outputs")
+COMPARE_JSON_FILE_PATH = os.path.join(TEST_PROJECT_PATH, "compare.json")
+YOUR_PYTHON_COMMAND = "python3"
+
+def compare_outputs_python(outputs_dir, inputs_dir):
+    print(RESULTS_PATH)
+    # open results file
+    results_filename = "spkmeans_compare_results_{}.txt".format(datetime.now().strftime("%d-%m-%Y_%H-%M-%S"))
+    results_path = os.path.join(TEST_RESULTS_PATH, results_filename)
+    with open(results_path, "w") as results_f:
+        # load json data
+        with open(COMPARE_JSON_FILE_PATH, "r") as f:
+            json_data = json.load(f)
+
+        # change current dir
+        os.chdir(FINAL_PROJECT_PATH)
+
+        for test_file in json_data:
+            input_file_relative_name = test_file["input_file_relative_name"]
+            input_file_path = os.path.join(INPUTS_PATH, input_file_relative_name)
+            tests = test_file["tests"]
+            for test in tests:
+                command_no_path = test["command"].replace("python", YOUR_PYTHON_COMMAND)
+                output_file_relative_path = test["output_file_relative_path"]
+                output_file_path = os.path.join(YOUR_RESULTS_PATH, output_file_relative_path)
+                command = command_no_path.format(input_file_path)
+                os.system('{} > "{}"'.format(command, output_file_path))
+
+                # compare results
+                with open(output_file_path, "r") as f:
+                    your_res = f.read()
+                with open(os.path.join(RESULTS_PATH, output_file_relative_path), "r") as f:
+                    our_res = f.read()
+                if (your_res != our_res):
+                    out_str = "### X ### the result of the command: {}, is not identical!".format(command)
+                    results_f.write(out_str + "\n")
+                    print(out_str)
+                else:
+                    out_str = "### V ### the result of the command: {}, is identical.".format(command)
+                    results_f.write(out_str + "\n")
+                    print(out_str)
+
+def main():
+    compare_outputs_python(YOUR_RESULTS_PATH, INPUTS_PATH)
+
+if __name__ == "__main__":
+    main()
